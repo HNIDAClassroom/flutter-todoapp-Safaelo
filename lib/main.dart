219516ccc7b2b36aa6a6585297f0719952c4e123
safae <louahsafae2000@gmail.com>
2023-10-13 16:35:45 +0100

    création du modèle (la classe task.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todolist_app/tasks.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(const MaterialApp(
+    home:Tasks(),
+  ),
+  );
 }
 
--- lib/tasks.dart
+++ lib/tasks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist_app/models/task.dart';
 
 class Tasks extends StatefulWidget {
   const Tasks({super.key});
@@ -10,6 +11,28 @@ class Tasks extends StatefulWidget {
 }
 
 class _TasksState extends State<Tasks> {
+final List<Task> _registeredTasks = [
+  Task(
+    title: 'Apprendre Flutter',
+    description: 'Suivre le cours pour apprendre de nouvelles compétences',
+    date: DateTime.now(),
+    category: Category.work,
+  ),
+  Task(
+    title: 'Faire les courses',
+    description: 'Acheter des provisions pour la semaine',
+    date: DateTime.now().subtract(Duration(days: 1)),
+    category: Category.shopping,
+  ),
+  Task(
+    title: 'Rediger un CR',
+    description: '',
+    date: DateTime.now().subtract(Duration(days: 2)),
+    category: Category.personal,
+  ),
+  // Add more tasks with descriptions as needed
+];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
